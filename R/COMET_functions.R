8d48e8b5393b82ff07e91d2279ea81a09952d6e1
Annice <62211977+AnniceNajafi@users.noreply.github.com>
2023-11-17 16:43:14 -0600

    Fixing global variable issue

--- R/COMET_functions.R
+++ R/COMET_functions.R
@@ -27,7 +27,8 @@ start_pipeline<-function(tables.dir, input.data.dir){
   my_package_env$emt.color.scheme.bold <- c("#24A19C","#D96098",  "#BEAEE2")
 
   flow.dat <- read.csv(system.file("extdata", "markov_flow.csv", package = "RCOMET"))
-  my_package_env$flow.dat <- flow.dat[1:7, ]
+  flow.dat[1:7, ] -> flow.dat
+  my_package_env$flow.dat <- flow.dat
 
   my_package_env$inc_num <- 0.04
   my_package_env$CTMC.scale <- 3/20
